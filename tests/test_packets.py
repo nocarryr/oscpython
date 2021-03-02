2f7a75cb1f165f9fd70ba659f00891e895f15fca
nocarryr <matt@nomadic-recording.com>
2021-03-02 11:56:31 -0600

    Add test for Bundle

--- tests/test_packets.py
+++ tests/test_packets.py
@@ -1,4 +1,5 @@
 import datetime
+import struct
 import pytest
 
 from oscpython import (
@@ -15,6 +16,19 @@ def message_args():
     typetags_expected = b',ifsbTFNIrt\x00'
     return arg_values, typetags_expected
 
+def check_message_args(msg: Message, all_arg_bytes: bytes):
+    for arg in msg.arguments:
+        try:
+            bytes_expected = arg.build_packet()
+        except ValueError as exc:
+            if 'Cannot pack empty argument' in str(exc):
+                continue
+            raise
+        arg_bytes = all_arg_bytes[:len(bytes_expected)]
+        print(f'arg={arg}: bytes_expected={bytes_expected}, arg_bytes={arg_bytes}')
+        assert arg_bytes == bytes_expected
+        all_arg_bytes = all_arg_bytes[len(bytes_expected):]
+
 def test_message_arguments(message_args):
     address = '/foo'
     arg_values, typetags_expected = message_args
@@ -29,15 +43,71 @@ def test_message_arguments(message_args):
     assert typetag_bytes == typetags_expected
     all_arg_bytes = msg_bytes[typetags_length+8:]
     print(f'all_arg_bytes = {all_arg_bytes}')
+    check_message_args(msg, all_arg_bytes)
 
-    for arg in msg.arguments:
-        try:
-            bytes_expected = arg.build_packet()
-        except ValueError as exc:
-            if 'Cannot pack empty argument' in str(exc):
-                continue
-            raise
-        arg_bytes = all_arg_bytes[:len(bytes_expected)]
-        print(f'arg={arg}: bytes_expected={bytes_expected}, arg_bytes={arg_bytes}')
-        assert arg_bytes == bytes_expected
-        all_arg_bytes = all_arg_bytes[len(bytes_expected):]
+
+def test_bundle(message_args):
+    arg_values, typetags_expected = message_args
+    typetags_length = len(typetags_expected)
+
+    bun = Bundle()
+    assert bun.timetag == TimeTag.Immediately
+    assert bun.timetag.seconds == 0
+    assert bun.timetag.fraction == 1
+
+    messages = (
+        Message.create('/foo', *arg_values),
+        Message.create('/bar/baz', *arg_values),
+        Message.create('/no/args'),
+    )
+    msg_addrs = (
+        b'/foo\x00\x00\x00\x00',
+        b'/bar/baz\x00\x00\x00\x00',
+        b'/no/args\x00\x00\x00\x00',
+    )
+
+    for msg in messages:
+        bun.add_packet(msg)
+
+    for i, msg in enumerate(messages):
+        assert msg.parent_bundle is bun
+        assert msg.parent_index == i
+
+    bun_bytes = bun.build_packet()
+    print(f'bun_bytes={bun_bytes}')
+    assert len(bun_bytes) % 4 == 0
+    assert bun_bytes.startswith(b'#bundle\x00')
+
+    tt_bytes = bun_bytes[8:16]
+    tt_val = struct.unpack('>q', tt_bytes)[0]
+    assert TimeTag.from_uint64(tt_val) == bun.timetag == TimeTag.Immediately
+    assert tt_val == 1
+
+    payload_bytes = bun_bytes[16:]
+    all_msg_bytes = []
+
+    while len(payload_bytes):
+        msg_size = struct.unpack('>i', payload_bytes[:4])[0]
+        print(f'msg_size={msg_size}')
+        payload_bytes = payload_bytes[4:]
+        msg_bytes = payload_bytes[:msg_size]
+        payload_bytes = payload_bytes[msg_size:]
+        all_msg_bytes.append(msg_bytes)
+
+    assert len(all_msg_bytes) == len(messages)
+
+    for msg, addr, msg_bytes in zip(messages, msg_addrs, all_msg_bytes):
+        print(f'msg={msg}, msg_bytes={msg_bytes}')
+        assert msg_bytes.startswith(addr)
+        if msg.address.pattern == '/no/args':
+            assert msg_bytes == addr
+        else:
+            start_ix = len(addr)
+            end_ix = start_ix + len(typetags_expected)
+            typetag_bytes = msg_bytes[start_ix:end_ix]
+            assert typetag_bytes == typetags_expected
+            start_ix = end_ix
+            all_arg_bytes = msg_bytes[start_ix:]
+            check_message_args(msg, all_arg_bytes)
+
+        assert msg.build_packet() == msg_bytes
