80a83f5ad655279be343874820a62aa77007c864
nocarryr <matt@nomadic-recording.com>
2021-03-02 10:43:18 -0600

    Avoid '{expr=}' f-strings for Py3.7 compat

--- tests/test_packets.py
+++ tests/test_packets.py
@@ -18,12 +18,13 @@ def test_message_arguments():
 
     msg = Message.create(address, *arg_values)
     msg_bytes = msg.build_packet()
+    assert len(msg_bytes) % 4 == 0
     address_bytes = msg_bytes[:8]
     assert address_bytes[:len(address)].decode() == msg.address.pattern == address
     typetag_bytes = msg_bytes[8:typetags_length+8]
     assert typetag_bytes == typetags_expected
     all_arg_bytes = msg_bytes[typetags_length+8:]
-    print(f'{all_arg_bytes=}')
+    print(f'all_arg_bytes = {all_arg_bytes}')
 
     for arg in msg.arguments:
         try:
@@ -33,6 +34,6 @@ def test_message_arguments():
                 continue
             raise
         arg_bytes = all_arg_bytes[:len(bytes_expected)]
-        print(f'{arg=}: {bytes_expected=}, {arg_bytes=}')
+        print(f'arg={arg}: bytes_expected={bytes_expected}, arg_bytes={arg_bytes}')
         assert arg_bytes == bytes_expected
         all_arg_bytes = all_arg_bytes[len(bytes_expected):]
