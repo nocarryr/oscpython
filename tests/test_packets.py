50f031409270796b8d984cd76d2640ff75bb7536
nocarryr <matt@nomadic-recording.com>
2021-03-04 13:28:39 -0600

    Add fuzz test using random addresses and arguments

--- tests/test_packets.py
+++ tests/test_packets.py
@@ -17,6 +17,57 @@ def message_args():
     typetags_expected = b',ifsbTFNIrt\x00'
     return arg_values, typetags_expected
 
+@pytest.fixture
+def message_addresses(faker):
+    n_levels = 7
+    n_branches = 3
+
+    def build(parent=None, depth=0):
+        parts = faker.words(n_branches)
+        for part in parts:
+            if parent is None:
+                address = f'/{part}'
+            else:
+                address = '/'.join([parent, part])
+            yield address
+            if depth < n_levels:
+                yield from build(address, depth+1)
+    return (tuple(build()))
+
+@pytest.fixture
+def random_arguments(faker):
+    arg_tags = list(arguments.ARGUMENTS_BY_TAG.keys())
+    num_tags = len(arg_tags)
+    def gen_args():
+        while True:
+            tag_ix = faker.pyint(max_value=num_tags-1)
+            arg_cls = arguments.ARGUMENTS_BY_TAG[arg_tags[tag_ix]]
+            if arg_cls.tag in ['T', 'F', 'N', 'I']:
+                yield arg_cls()
+                continue
+            if arg_cls is arguments.Int32Argument:
+                value = faker.pyint(min_value=arguments.INT32_MIN, max_value=arguments.INT32_MAX)
+            elif arg_cls is arguments.Int64Argument:
+                value = faker.pyint(min_value=arguments.INT64_MIN, max_value=arguments.INT64_MAX)
+            elif arg_cls.py_type is float:
+                value = faker.pyfloat()
+            elif arg_cls is arguments.StringArgument:
+                value = faker.sentence()
+            elif arg_cls is arguments.CharArgument:
+                continue
+                value = faker.pystr(max_chars=1)
+            elif arg_cls is arguments.BlobArgument:
+                length = faker.pyint(min_value=1, max_value=255)
+                value = faker.binary(length=length)
+            elif arg_cls is arguments.TimeTagArgument:
+                value = faker.date_time()
+            elif arg_cls is arguments.RGBArgument:
+                rgba = {k:faker.pyint(max_value=255) for k in ['r','g','b','a']}
+                value = ColorRGBA(**rgba)
+            yield arg_cls(value=value)
+    return gen_args()
+
+
 def check_message_args(msg: Message, all_arg_bytes: bytes):
     for arg in msg.arguments:
         try:
@@ -134,3 +185,28 @@ def test_bundle(message_args):
     assert len(parsed.packets) == len(bun.packets)
     for parsed_pkt, bun_pkt in zip(parsed.packets, bun.packets):
         check_parsed_message(bun_pkt, parsed_pkt)
+
+
+def test_random_addrs_and_args(message_addresses, random_arguments):
+    args_per_message = 4
+    messages_per_bundle = 8
+    print('num_addresses: {}'.format(len(message_addresses)))
+    bun = None
+    for address in message_addresses:
+        if bun is None:
+            bun = Bundle()
+
+        arg_values = [next(random_arguments) for _ in range(args_per_message)]
+        msg = Message.create(address, *arg_values)
+        bun.add_packet(msg)
+        if len(bun.packets) == messages_per_bundle:
+            bun_bytes = bun.build_packet()
+
+            parsed, remaining = Packet.parse(bun_bytes)
+            # print(f'parsed={parsed}, remaining={remaining}')
+            assert parsed.timetag == bun.timetag
+            assert len(parsed.packets) == len(bun.packets)
+            for parsed_pkt, bun_pkt in zip(parsed.packets, bun.packets):
+                check_parsed_message(bun_pkt, parsed_pkt)
+
+            bun = None
